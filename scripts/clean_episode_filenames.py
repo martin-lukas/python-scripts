7b1d3fcd3dd73dceaa602e450857f8fe6f7f07d3
Martin Lukas <martin.lukas@email.com>
2024-01-24 11:06:04 +0000

    CleanEpisodeFilenames: reorder functions by call order

--- scripts/clean_episode_filenames.py
+++ scripts/clean_episode_filenames.py
@@ -5,6 +5,16 @@ import sys
 SEASON_EPISODE_PATTERN = re.compile(r'(S\d{2}E\d{2})')
 
 
+def process_files(dir_path, tv_show_name):
+    try:
+        files = [f for f in dir_path.iterdir() if f.is_file()]
+        for file in files:
+            new_name = clean_episode_name(file.name, tv_show_name)
+            rename_file(file, f"{new_name}{file.suffix}")
+    except FileNotFoundError:
+        print(f"The specified folder '{dir_path}' does not exist.")
+
+
 def clean_episode_name(name, tv_show_name):
     season_and_episode = SEASON_EPISODE_PATTERN.search(name)
     if season_and_episode:
@@ -19,16 +29,6 @@ def rename_file(file_path, new_name):
         print(f'Renamed {file_path.name} to {new_name}')
     except Exception as e:
         print(f'Error renaming {file_path.name}: {e}')
-
-
-def process_files(dir_path, tv_show_name):
-    try:
-        files = [f for f in dir_path.iterdir() if f.is_file()]
-        for file in files:
-            new_name = clean_episode_name(file.name, tv_show_name)
-            rename_file(file, f"{new_name}{file.suffix}")
-    except FileNotFoundError:
-        print(f"The specified folder '{dir_path}' does not exist.")
 
 
 if __name__ == "__main__":
